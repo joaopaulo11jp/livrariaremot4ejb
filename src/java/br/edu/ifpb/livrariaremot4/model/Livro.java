f9d432a92d08eed50c77e403a374bc1ccec24590
joaopaulo11jp <joaopaulo11jp@gmail.com>
2016-03-06 09:26:49 -0300

    First Commit

---
+++ src/java/br/edu/ifpb/livrariaremot4/model/Livro.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.livrariaremot4.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author joaopaulo
+ */
+public class Livro implements Serializable{
+    private Long codigo;
+    private String titulo;
+    private String editora;
+    private String isbn;
+    private String edicao;
+    private String autor;
+    
+        public Livro(Long codigo, String titulo, String editora, String isbn, 
+                String edicao, String autor){
+            this.codigo = codigo;
+            this.editora = editora;
+            this.isbn = isbn;
+            this.edicao = edicao;
+            this.autor = autor;
+        }
+        
+        public Livro(){}
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+}
