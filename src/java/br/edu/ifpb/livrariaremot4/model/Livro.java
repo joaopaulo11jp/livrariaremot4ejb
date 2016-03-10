dde768f986e5094df9314539b99c281ee1aa644e
joaopaulo11jp <joaopaulo11jp@gmail.com>
2016-03-09 23:20:45 -0300

    Auto increment id...

--- src/java/br/edu/ifpb/livrariaremot4/model/Livro.java
+++ src/java/br/edu/ifpb/livrariaremot4/model/Livro.java
@@ -7,6 +7,8 @@ package br.edu.ifpb.livrariaremot4.model;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -17,6 +19,7 @@ import javax.persistence.Id;
 @Entity
 public class Livro implements Serializable{
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long codigo;
     private String titulo;
     private String editora;
