3e39df399695a9fc0d0238993d3d860f2dbbb096
joaopaulo11jp <joaopaulo11jp@gmail.com>
2016-03-11 22:36:02 -0300

    Some fixes in ManagerBean

--- src/java/br/edu/ifpb/livrariaremot4/bean/LivroManagerBean.java
+++ src/java/br/edu/ifpb/livrariaremot4/bean/LivroManagerBean.java
@@ -7,13 +7,15 @@ package br.edu.ifpb.livrariaremot4.bean;
 
 import br.edu.ifpb.livrariaremot4.interfaces.LivroManagerBeanRemote;
 import br.edu.ifpb.livrariaremot4.model.Livro;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 
 /**
  *
  * @author joaopaulo
  */
-@Stateless
+@Stateless(mappedName = "LivroManager")
 public class LivroManagerBean implements LivroManagerBeanRemote{
 
 
@@ -43,12 +45,26 @@ public class LivroManagerBean implements LivroManagerBeanRemote{
     }
 
     @Override
-    public Livro cadastrar(String nome, String titulo, String editora, String isbn, String edicao, String autor) {
+    public Livro alterarLivro(Livro livro) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Livro alterarLivro(Livro livro) {
+    public String teste() {
+        return "Teste";
+    }
+
+    @Override
+    public List<Livro> getTodosOsLivros() {
+        List<Livro> l = new ArrayList<Livro>();
+        l.add(new Livro(new Long(1), "Teste 1", "Editora 1", "1234", "vol 1", "autor 1"));
+        l.add(new Livro(new Long(2), "Teste 2", "Editora 1", "1234", "vol 1", "autor 1"));
+        
+        return l;
+    }
+
+    @Override
+    public Livro cadastrar(String titulo, String editora, String isbn, String edicao, String autor) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
--- src/java/br/edu/ifpb/livrariaremot4/interfaces/LivroManagerBeanRemote.java
+++ src/java/br/edu/ifpb/livrariaremot4/interfaces/LivroManagerBeanRemote.java
@@ -6,6 +6,7 @@
 package br.edu.ifpb.livrariaremot4.interfaces;
 
 import br.edu.ifpb.livrariaremot4.model.Livro;
+import java.util.List;
 import javax.ejb.Remote;
 
 /**
@@ -15,11 +16,13 @@ import javax.ejb.Remote;
 
 @Remote
 public interface LivroManagerBeanRemote {
-    Livro cadastrar(String nome, String titulo, String editora, String isbn, String edicao, String autor);
+    Livro cadastrar(String titulo, String editora, String isbn, String edicao, String autor);
     Livro alterarLivro(Livro livro);
+    List<Livro> getTodosOsLivros();
     Livro consultarPorAutor(String autor);
     Livro consultarPorISBN(String isbn);
     Livro consultarPorTitulo(String titulo);
     Livro remover(Long id);
     Integer numeroBuscas();
+    String teste();
 }
